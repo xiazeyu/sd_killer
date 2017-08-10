e6f9f0cf0fd6e526378feb155400eaacac1828d0
xiazeyu_2011 <xiazeyu_2011@126.com>
2017-08-10 21:12:04 +0800

    Update program ver 1.0

---
+++ main.cpp
@@ -0,0 +1,21 @@
+/*
+
+	Shutdown Killer v1.0 By Xia
+
+*/
+
+// #include <cstdio>
+#include <cstdlib>
+
+using namespace std;
+
+int main(){
+	
+	while(1){
+		system("shutdown -a");
+	}
+	
+    return 0;
+
+}
+
