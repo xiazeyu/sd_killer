c61cd474c6f86902a6f3fe0869ff53801b724c2e
xiazeyu_2011 <xiazeyu_2011@126.com>
2017-08-10 21:12:36 +0800

    Use thread tech ver 2.0

--- main.cpp
+++ main.cpp
@@ -1,20 +1,57 @@
 /*
 
-	Shutdown Killer v1.0 By Xia
+	Shutdown Killer v2.0 By Xia
 
 */
 
-// #include <cstdio>
+#include <cstdio>
 #include <cstdlib>
+#include <iostream>
 
 using namespace std;
 
-int main(){
-	
+#include <thread>
+using std::thread;
+#include <vector>
+using std::vector;
+
+
+struct ThreadItem {
+	thread worker;
+};
+
+void* ThreadFunction() {
+
 	while(1){
-		system("shutdown -a");
+
+		system("shutdown -a"); // Command Here!
+		// printf("from thread ID %d\n",std::this_thread::get_id());
+		// cout << "from thread ID " << std::this_thread::get_id() << endl;
 	}
-	
+
+}
+
+int main(){
+
+	// Get the amount of "processing units"
+	int n = std::thread::hardware_concurrency();
+
+	// Create array of threads
+	vector<ThreadItem> threadlist;
+	threadlist.resize(n);
+
+	// Spawn a thread for each core
+	for(int i = 0;i < n ;i++) {
+		threadlist[i].worker = thread(ThreadFunction);
+	}
+
+
+	// Wait for them all to finish
+	for(int i = 0;i < n;i++) {
+		threadlist[i].worker.join();
+	}
+
+
     return 0;
 
 }
